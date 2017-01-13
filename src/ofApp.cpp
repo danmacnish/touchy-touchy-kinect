b352bac29d39f29460be14759e57e4403ad4301f
whiteboarddan <macnish.daniel@gmail.com>
2017-01-13 21:18:09 +1100

    display Kinect depth image on screen (pixel values take range 0-255)

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,56 @@
 #include "ofApp.h"
 
+//Connect to Kinect
+//Get the latest depth image and assign to openCV grayscale image
+//display to screen
+
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    kinect.init();
+    kinect.open();
+    
+    //log that we are connected
+    if(kinect.isConnected()) {
+        ofLogNotice() << "connected to Kinect";
+    } else {
+        ofLogNotice() << "failed to connect";
+    };
+    
+    //allocate mem for depth image
+    depthImage.allocate(kinect.width, kinect.height);
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    ofBackground(100, 100, 100);
+    
+    kinect.update();
+    
+    //if there is a new depth frame available
+    if(kinect.isFrameNewDepth()) {
+        //get the depth frame
+        depthImage.setFromPixels(kinect.getDepthPixels());
+    }
+    
+    //update cv images
+    //depthImage.flagImageChanged();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofSetColor(255, 255, 255);
+    
+    depthImage.draw(10,10,400,300);
 
+}
+
+//exit-------------------------------------------------
+
+void ofApp::exit() {
+    kinect.setCameraTiltAngle(0);
+    kinect.close();
 }
 
 //--------------------------------------------------------------
